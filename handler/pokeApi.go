f6a2cdf27368ffd724ebc456e83dd4d045880e7c
Christian <3003html@gmail.com>
2022-08-24 02:45:33 -0500

    function respond with json added

---
+++ handler/pokeApi.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// Respond to client with json
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	_, err = w.Write(response)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
