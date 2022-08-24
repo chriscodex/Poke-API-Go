13cae985d8e73655f3291737fb8ca395635f08bb
Christian <3003html@gmail.com>
2022-08-24 02:50:13 -0500

    endpoint pokemon added

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/ChrisCodeX/Poke-API-Go/handler"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/pokemon/{id}", handler.HandlerGetPokemon).Methods(http.MethodGet)
 
 }
