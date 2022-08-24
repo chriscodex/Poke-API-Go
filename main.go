cddf7b814345af711ce3d81a4b3c105313a100a9
Christian <3003html@gmail.com>
2022-08-24 02:49:39 -0500

    router created with mux library

--- main.go
+++ main.go
@@ -1,20 +1,10 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/pokemon/pikachu").Methods(http.MethodGet)
-
-	err := http.ListenAndServe(":8080", router)
-
-	if err != nil {
-		log.Println("Error found")
-	}
 }
