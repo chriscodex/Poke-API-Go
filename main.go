eacd7f1e05e7f72b745df81e1ac6294bdbc65aed
Christian <3003html@gmail.com>
2022-08-24 23:55:54 -0500

    function listen and serve added

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	router := mux.NewRouter()
 
+	router.HandleFunc("/pokemon/pikachu").Methods(http.MethodGet)
+
+	err := http.ListenAndServe(":8080", router)
+
+	if err != nil {
+		log.Println("Error found")
+	}
 }
