88bb1c0d9d7187522dcf4f9f6bfa871f5e0bae16
Christian <3003html@gmail.com>
2022-08-25 02:59:27 -0500

    AllowedAbilities var created in models.go

--- models/models.go
+++ models/models.go
@@ -6,3 +6,10 @@ type Pokemon struct {
 	Power     string         `json:"Type"`
 	Abilities map[string]int `json:"Abilities"`
 }
+
+var AllowedAbilities = map[string]string{
+	"hp":      "Hp",
+	"attack":  "Attack",
+	"defense": "Defense",
+	"speed":   "Speed",
+}
