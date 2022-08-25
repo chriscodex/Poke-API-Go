2259b9a8580eed33cca4ef90331062994787536f
Christian <3003html@gmail.com>
2022-08-25 02:58:50 -0500

    Power and Abilities attributes added in pokemon struct

--- models/models.go
+++ models/models.go
@@ -1,6 +1,8 @@
 package models
 
 type Pokemon struct {
-	Id   int    `json:"Id"`
-	Name string `json:"Name"`
+	Id        int            `json:"Id"`
+	Name      string         `json:"Name"`
+	Power     string         `json:"Type"`
+	Abilities map[string]int `json:"Abilities"`
 }
