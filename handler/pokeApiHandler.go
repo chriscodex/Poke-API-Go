8e5c8c440510d6dd3b0c937ca5afb3e9dbacf3c5
Christian <3003html@gmail.com>
2022-08-26 23:22:18 -0500

    validations added to getPokemonFormPokeApi function

--- handler/pokeApiHandler.go
+++ handler/pokeApiHandler.go
@@ -3,8 +3,12 @@ package handler
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/ChrisCodeX/Poke-API-Go/models"
 )
 
 var (
@@ -25,5 +29,28 @@ func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 	_, err = w.Write(response)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func GetPokemonFromPokeApi(id string) (*models.PokeApiPokemonResponse, error) {
+	request := fmt.Sprintf("http://pokeapi.co/api/v2/pokemon/%s", id)
+
+	response, err := http.Get(request)
+	if err != nil {
+		return nil, err
+	}
+
+	/* 	Getting the status code from the pokeapi and validate*/
+	if response.StatusCode == http.StatusNotFound {
+		return nil, ErrPokemonNotFound
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return nil, ErrPokeApiFailure
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
 	}
 }
